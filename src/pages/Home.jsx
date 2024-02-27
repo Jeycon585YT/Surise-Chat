6eceeb60f6960cca3c5576f360a877381f121d48
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:16:15 +0300

    Add SideBar and Chat components to the Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import SideBar from "../components/SideBar";
+import Chat from "../components/Chat";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-sky-200 h-screen flex items-center justify-center">
+      <div className="border border-white rounded-lg w-8/12 h-4/5 flex">
+        <SideBar />
+        <Chat />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
