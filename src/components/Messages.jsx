bf06ad3ba2cf19422acf750ee0c49851d35a2041
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 23:33:01 +0300

    Add Message component and render it six times in the Messages component.

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,5 +1,16 @@
+import Message from "./Message";
+
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <div className="bg-gray-200 p-4 h-[calc(100%-144px)] overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 };
 
 export default Messages;
