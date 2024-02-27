6e6a49670d4a228c88c1392edc4fa0ef4e0516c7
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 19:09:18 +0300

    Update button component styles

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ className, text, ...props }) => {
+  return (
+    <>
+      <button
+        className={`${className}  text-white py-2 px-4 font-bold hover:brightness-125 duration-150`}
+        {...props}
+      >
+        {text}
+      </button>
+    </>
+  );
 };
 
 export default Button;
