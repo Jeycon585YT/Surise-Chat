ebd32c3caf7ac0dbb4adcc9a0d9e24b5648752ff
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:15:53 +0300

    Add Navbar, Search, and Chats components to the sidebar.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,15 @@
+import Navbar from "../components/Navbar";
+import Search from "../components/Search";
+import Chats from "../components/Chats";
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="basis-1/3 bg-amber-950 rounded-lg rounded-r-none">
+      <Navbar />
+      <Search />
+      <Chats />
+    </div>
+  );
 };
 
 export default SideBar;
