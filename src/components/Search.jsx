94db8aa40aae83830e3298f978ae8d9531e3810e
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:13:57 +0300

    Add search bar to the top of the page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,21 @@
+import { CiSearch } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    // serach
+    <div className="border-b border-slate-400">
+      {/*  search form */}
+      <div className="relative">
+        <input
+          type="text"
+          className="px-10 py-4 w-full bg-transparent border-none text-white outline-none placeholder:text-slate-100/40"
+          placeholder="Find a user"
+        />
+        <FaSearch className="absolute top-4 left-2 text-slate-100/40 text-xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
