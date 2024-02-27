84bc0176122fc2934b708148d700b7462169385a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:15:14 +0300

    Refactor codebase to use new components: Navbar, Search, Chats, and Chat.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,55 @@
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div>
+      <div className="flex p-2 items-center gap-2 text-white hover:bg-black/40 cursor-pointer">
+        <img
+          className="w-12 h-12 rounded-full object-cover object-center"
+          src="https://sm.ign.com/ign_tr/cover/h/harry-pott/harry-potter-the-series_p1mc.jpg"
+          alt=""
+        />
+        <div>
+          <span className="font-bold text-lg ">Potter</span>
+          <p className="text-slate-100">Hello</p>
+        </div>
+      </div>
+
+      <div className="flex p-2 items-center gap-2 text-white hover:bg-black/40 cursor-pointer">
+        <img
+          className="w-12 h-12 rounded-full object-cover object-center"
+          src="https://sm.ign.com/ign_tr/cover/h/harry-pott/harry-potter-the-series_p1mc.jpg"
+          alt=""
+        />
+        <div>
+          <span className="font-bold text-lg ">Potter</span>
+          <p className="text-slate-100">Hello</p>
+        </div>
+      </div>
+
+      <div className="flex p-2 items-center gap-2 text-white hover:bg-black/40 cursor-pointer">
+        <img
+          className="w-12 h-12 rounded-full object-cover object-center"
+          src="https://sm.ign.com/ign_tr/cover/h/harry-pott/harry-potter-the-series_p1mc.jpg"
+          alt=""
+        />
+        <div>
+          <span className="font-bold text-lg ">Potter</span>
+          <p className="text-slate-100">Hello</p>
+        </div>
+      </div>
+
+      <div className="flex p-2 items-center gap-2 text-white hover:bg-black/40 cursor-pointer">
+        <img
+          className="w-12 h-12 rounded-full object-cover object-center"
+          src="https://sm.ign.com/ign_tr/cover/h/harry-pott/harry-potter-the-series_p1mc.jpg"
+          alt=""
+        />
+        <div>
+          <span className="font-bold text-lg ">Potter</span>
+          <p className="text-slate-100">Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
