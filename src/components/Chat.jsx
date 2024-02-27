0c4cb3b149191418909c43efc364a2fb10600b60
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:14:45 +0300

    Add chat icons to the chat header.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,22 @@
+import { FaUserPlus } from "react-icons/fa6";
+import { BsThreeDots } from "react-icons/bs";
+import { BsCameraVideoFill } from "react-icons/bs";
+
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="basis-2/3 ">
+      {/* Chat info */}
+      <div className="h-20 bg-amber-950 text-white flex justify-between items-center p-2">
+        <span className="font-medium text-slate-100 text-xl">Zurafa</span>
+        {/* chat Icons */}
+        <div className="flex text-4xl space-x-4">
+          <BsCameraVideoFill className="cursor-pointer hover:text-sky-200 duration-150" />
+          <FaUserPlus className="cursor-pointer hover:text-sky-200 duration-150" />
+          <BsThreeDots className="cursor-pointer hover:text-sky-200 duration-150" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
