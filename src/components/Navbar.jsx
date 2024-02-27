ea7de6e651f19efd02d0eca4f718d21d4f3db87c
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 22:13:30 +0300

    Update the navbar with a logo, profile image, username, and logout button.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import Button from "./shared/Button";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center bg-black/40 rounded-lg rounded-r-none h-20 p-2 justify-between text-slate-50">
+      <span className="font-black text-xl">Logo</span>
+      <div className="flex items-center gap-2">
+        <img
+          className="bg-slate-50 h-6 w-6 rounded-full object-cover"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Muhammad_Ali_NYWTS.jpg/1200px-Muhammad_Ali_NYWTS.jpg"
+          alt="profile image"
+        />
+        <span className="">Muhammed Ali</span>
+        <Button className="bg-amber-950 text-sm font-medium" text="Logout" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
