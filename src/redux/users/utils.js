791f444f202f73b6e187c9c38157731cb7b745cb
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-28 17:34:54 +0300

    Update `setUsersToRedux` to use `setUsers` action.

--- src/redux/users/utils.js
+++ src/redux/users/utils.js
@@ -1,6 +1,6 @@
-import { setUser } from "../auth/authSlice";
 import { store } from "../store";
+import { setUsers } from "./usersSlice";
 
 export const setUsersToRedux = (usersData) => {
-  store.dispatch(setUser(usersData));
+  store.dispatch(setUsers(usersData));
 };
