f400178a86cb9be723af9f10e07eb590c7458987
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-28 03:53:11 +0300

    Add Firebase registration functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,39 @@
 import { LuImagePlus } from "react-icons/lu";
 import Input from "../components/shared/Input";
 import Button from "../components/shared/Button";
+import {
+  registerUserToFirebase,
+  updateProfileImageToFirebase,
+  updateUserProfileToFirebase,
+} from "../Firebase/actions";
+import { handleFirebaseError } from "../Firebase/errorHandle";
 
 const Register = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const user = await registerUserToFirebase(email, password);
+      const photoURL = await updateProfileImageToFirebase(file, user.uid);
+      await updateUserProfileToFirebase({
+        displayName,
+        photoURL,
+      });
+    } catch (error) {
+      handleFirebaseError(error);
+    }
+  };
+
   return (
     <div className="bg-sky-200 min-h-screen flex items-center justify-center">
       <div className="bg-white py-5 px-16 rounded-lg flex flex-col gap-4 items-center">
         <h1 className="text-neutral-600 font-bold text-4xl">Logo</h1>
         <h4 className="text-neutral-600 text-lg">Register</h4>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <Input placeholder="display name" type="text" />
           <Input placeholder="email" type="email" />
           <Input placeholder="password" type="password" />
