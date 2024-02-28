b7337a2692a463f18b6c58e9ab149b4c1a9544ae
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-28 03:52:38 +0300

    Update Firebase actions to include profile image upload and user profile update.

--- src/Firebase/actions.js
+++ src/Firebase/actions.js
@@ -1,7 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "./config";
 import { handleFirebaseError } from "./errorHandle";
 import toast from "react-hot-toast";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export const registerUserToFirebase = async (email, password) => {
   try {
@@ -14,6 +15,29 @@ export const registerUserToFirebase = async (email, password) => {
     toast.success("You have successfully registered!");
 
     return userCredential?.user;
+  } catch (error) {
+    handleFirebaseError(error);
+    return false;
+  }
+};
+
+export const updateProfileImageToFirebase = async (file, uid) => {
+  try {
+    const storageRef = ref(storage, "images/" + uid);
+
+    const snapshot = await uploadBytes(storageRef, file);
+    const photoURL = await getDownloadURL(snapshot.ref);
+    return photoURL;
+  } catch (error) {
+    handleFirebaseError(error);
+    return false;
+  }
+};
+
+export const updateUserProfileToFirebase = async (...data) => {
+  try {
+    await updateProfile(auth.currentUser, data);
+    return true;
   } catch (error) {
     handleFirebaseError(error);
     return false;
