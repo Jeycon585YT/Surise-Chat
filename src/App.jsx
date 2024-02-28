6832f500f80905e18d3030bdd8709a5a7106a2a6
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-28 03:53:31 +0300

    Add Register page and Firebase storage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
       <Toaster />
-      <Home />
+      <Register />
+      {/* <Home /> */}
     </>
   );
 };
