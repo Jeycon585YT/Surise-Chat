60cdfcba133e62017d4a6f282c65ff00e4a355b5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-01 06:42:05 +0300

    Improved MessageList component
    - Implemented auto-scrolling to the latest message when the chat is updated.
    - Integrated the @formkit/auto-animate library for smoother scrolling transitions.
    - Adjusted the layout to ensure better responsiveness on small screens.

--- src/components/chat/MessageList.jsx
+++ src/components/chat/MessageList.jsx
@@ -1,14 +1,39 @@
+import { useEffect } from "react";
 import Message from "./Message";
+import { useSelector } from "react-redux";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-const MessageList = () => {
+const MessageList = ({ scrollRef }) => {
+  const [parent] = useAutoAnimate();
+  const { chatRoms, currentChatId } = useSelector((state) => state.chat);
+
+  useEffect(() => {
+    if (!currentChatId) return;
+
+    const scrollToBottom = () => {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+
+    const scrollTimeout = setTimeout(scrollToBottom, 500);
+
+    return () => clearTimeout(scrollTimeout);
+  }, [currentChatId, scrollRef]);
+
   return (
-    <div className="bg-gray-200 p-4 h-[calc(100%-144px)] overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+    <div
+      ref={parent}
+      className="bg-gray-200 px-4 h-[calc(100%-144px)] max-sm:h-[calc(100%-114px)] overflow-auto"
+    >
+      {currentChatId &&
+        chatRoms[currentChatId]?.chatList.map((message, index, arr) => (
+          <Message
+            key={message.time}
+            arr={arr}
+            index={index}
+            message={message}
+          />
+        ))}
+      <span ref={scrollRef}></span>
     </div>
   );
 };
