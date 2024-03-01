8208b27140ce823d72c54883fc019ce97e3e1f87
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-01 03:16:41 +0300

    Refactor login page UI and form handling
    - Simplified form handling by extracting it into a separate function.
    - Updated styling for a cleaner and more visually appealing design.
    - Reorganized and improved the readability of the Login component.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,49 +1,56 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/shared/Input";
 import Button from "../components/shared/Button";
-import { loginToFirebase } from "../Firebase/actions";
-import { useState } from "react";
 import LoadingSpin from "../components/shared/LoadingSpin";
-import { Link, useNavigate } from "react-router-dom";
+import { loginToFirebase } from "../Firebase/actions";
 
 const Login = () => {
   const [isPending, setIsPending] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (email, password) => {
+    try {
+      await loginToFirebase(email, password);
+      setIsPending(false);
+      navigate("/");
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setIsPending(true);
 
     const email = e.target[0].value;
     const password = e.target[1].value;
 
-    try {
-      console.log(email, password);
-      await loginToFirebase(email, password);
-      setIsPending(false);
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
+    handleLogin(email, password);
   };
 
   return (
-    <div className="bg-sky-200 min-h-screen flex items-center justify-center">
+    <div className="bg-gradient-to-br from-blue-400 to-sky-300 min-h-screen flex items-center justify-center">
       {isPending && <LoadingSpin className="absolute" />}
-      <div className="bg-white py-5 px-16 rounded-lg flex flex-col gap-4 items-center">
-        <h1 className="text-neutral-600 font-bold text-4xl">Logo</h1>
-        <h4 className="text-neutral-600 text-lg">Login</h4>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <Input placeholder="email" type="email" />
-          <Input placeholder="password" type="password" />
-
-          <Button className="bg-sky-600/80" text="Sign In" />
+      <div className="bg-white py-8 px-10 rounded-lg shadow-lg flex flex-col gap-4 items-center">
+        <h1 className="text-neutral-800 font-bold text-4xl mb-4">Logo</h1>
+        <h4 className="text-neutral-700 text-lg mb-6">Login</h4>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-6 w-full max-w-md"
+        >
+          <Input placeholder="Email" type="email" />
+          <Input placeholder="Password" type="password" />
+          <Button
+            className="bg-blue-500 hover:bg-blue-600 text-white"
+            text="Sign In"
+          />
         </form>
-        <p className="mt-2 text-neutral-600">
-          You don&apos;t have an account?{" "}
+        <p className="mt-4 text-neutral-700">
+          Don't have an account?{" "}
           <Link
             to="/register"
-            className="text-sky-300 hover:text-sky-800 duration-150"
+            className="text-sky-500 hover:text-sky-800 duration-150"
           >
             Register
           </Link>
