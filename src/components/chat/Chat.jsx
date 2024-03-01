9c70b82041fe72c3c0a9d6555eafcff80c6c0857
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-01 12:49:03 +0300

    Extract ChatHeader component from Chat
    Moved the ChatHeader component into its own file, creating a separate component for better organization and maintainability within the Chat feature.

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,32 +1,9 @@
-import { FaUserPlus } from "react-icons/fa6";
-import { BsThreeDots, BsCameraVideoFill } from "react-icons/bs";
 import { AiOutlineMessage } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import { useRef } from "react";
-import photo from "../../assets/images/defaultPhoto.jpg";
 import MessageList from "./MessageList";
 import ChatInput from "./ChatInput";
-
-const ChatHeader = ({ photoURL, displayName }) => (
-  <div className="h-20 max-sm:h-12 bg-amber-950 text-white flex justify-between items-center p-4 sm:p-2">
-    <div className="flex items-center space-x-4">
-      <img
-        src={photoURL || photo}
-        alt=""
-        className="rounded-full h-12 w-12 max-sm:h-8 max-sm:w-8 object-cover object-center"
-      />
-      <span className="font-medium text-slate-100 text-lg sm:text-xl">
-        {displayName}
-      </span>
-    </div>
-
-    <div className="flex text-3xl space-x-4">
-      <BsCameraVideoFill className="cursor-pointer hover:text-sky-200 duration-150 max-sm:h-8 max-sm:w-8" />
-      <FaUserPlus className="cursor-pointer hover:text-sky-200 duration-150 max-sm:h-8 max-sm:w-8" />
-      <BsThreeDots className="cursor-pointer hover:text-sky-200 duration-150 max-sm:h-8 max-sm:w-8" />
-    </div>
-  </div>
-);
+import { ChatHeader } from "./ChatHeader";
 
 const Chat = () => {
   const scrollRef = useRef(null);
