e2dd4ceecd35734edd529042b472c4ed8c498aae
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-01 03:00:12 +0300

    Refactor layout in Home component for improved styling
    - Changed background gradient
    - Adjusted container dimensions
    - Added shadow to enhance visual appeal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import Chat from "../components/chat/Chat";
 
 const Home = () => {
   return (
-    <div className="bg-sky-200 h-screen flex items-center justify-center">
-      <div className="border border-white rounded-lg w-8/12 h-4/5 flex">
+    <div className="bg-gradient-to-br from-blue-400 to-sky-300 min-h-screen flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-lg max-sm:w-full w-8/12 h-4/5 flex">
         <SideBar />
         <Chat />
       </div>
